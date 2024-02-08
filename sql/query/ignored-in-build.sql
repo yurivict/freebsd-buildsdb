78904b373758560b9db111c865491da07181aaa2
Yuri Victorovich <yuri@FreeBSD.org>
2024-02-08 08:54:15 -0800

    Add query SQL scripts

---
+++ sql/query/ignored-in-build.sql
@@ -0,0 +1,16 @@
+SELECT
+	l.origin AS Port,
+	l.pkgname AS Package,
+	l.reason AS Reason
+FROM
+	masterbuild m,
+	build b,
+	ignored l
+WHERE
+	m.id = b.masterbuild_id
+	AND
+	b.id = l.build_id
+	AND
+	m.name = '%s'
+	AND
+	b.name = '%s'
